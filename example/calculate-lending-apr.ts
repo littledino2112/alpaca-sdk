dac7c92e78ac748600af1fcab4d31f689b6efec9
Almost Llamas <kim.tony.20.20.20@gmail.com>
2021-10-28 17:42:22 +0700

    feat: implement a working example

--- example/calculate-lending-apr.ts
+++ example/calculate-lending-apr.ts
@@ -1,16 +1,23 @@
-import { ethers, network } from "hardhat";
-import {
-    LendingPoolCalculator,
-    LendingPoolCalculatorFactory,
-  } from '@alpaca-finance/alpaca-sdk/build-cjs/libs/calculators/LendingPoolCalculator'
+// Always set the ENV as 'prod' before importing the package.
+// You can also consider using `dotenv` package to make this a bit cleaner.
+process.env.ENV = 'prod'
 
+import {
+  LendingPoolCalculatorFactory,
+} from '@alpaca-finance/alpaca-sdk/build-cjs/libs/calculators/LendingPoolCalculator'
 import { vaults } from '@alpaca-finance/alpaca-sdk/build-cjs/constants/vault'
+import { ethers } from 'ethers'
+import { formatEther } from '@ethersproject/units'
 
 async function main() {
-    const provider = ethers.provider;
-    const lendingPoolCalculators = await new LendingPoolCalculatorFactory(
-        provider,
-      ).create(...vaults)
+  const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org')
+  const lendingPoolCalculators = await new LendingPoolCalculatorFactory(
+    provider,
+  ).create(...vaults)
+  
+  lendingPoolCalculators.forEach(cal => {
+    console.log('Vault:', cal.vault.name, 'Lending APR:', formatEther(cal.yearlyLendingAPR))
+  })
 }
 
 main()
