8eb42343b461450e835b115a75549830eea08c80
ricardo.alpaca <ricardo.alpaca.619@gmail.com>
2021-11-29 13:58:55 +0700

    chore: remove unnecessary config from hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,37 +5,8 @@ import "@nomiclabs/hardhat-ethers";
 import "hardhat-typechain"
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "mainnet",
   networks: {
-    hardhat: {
-      chainId: 31337,
-      gas: 12000000,
-      blockGasLimit: 0x1fffffffffffff,
-      allowUnlimitedContractSize: true,
-      timeout: 1800000,
-      accounts: [
-        {
-          privateKey: process.env.LOCAL_PRIVATE_KEY_1,
-          balance: "10000000000000000000000",
-        },
-        {
-          privateKey: process.env.LOCAL_PRIVATE_KEY_2,
-          balance: "10000000000000000000000",
-        },
-        {
-          privateKey: process.env.LOCAL_PRIVATE_KEY_3,
-          balance: "10000000000000000000000",
-        },
-        {
-          privateKey: process.env.LOCAL_PRIVATE_KEY_4,
-          balance: "10000000000000000000000",
-        },
-      ],
-    },
-    testnet: {
-      url: "https://data-seed-prebsc-1-s3.binance.org:8545",
-      accounts: [process.env.BSC_TESTNET_PRIVATE_KEY],
-    },
     mainnet: {
       url: process.env.BSC_MAINNET_RPC,
       accounts: [process.env.BSC_MAINNET_PRIVATE_KEY],
@@ -46,35 +17,6 @@ module.exports = {
     deployer: {
       default: 0,
     },
-  },
-  solidity: {
-    version: "0.6.6",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1,
-      },
-      evmVersion: "istanbul",
-      outputSelection: {
-        "*": {
-          "": ["ast"],
-          "*": [
-            "evm.bytecode.object",
-            "evm.deployedBytecode.object",
-            "abi",
-            "evm.bytecode.sourceMap",
-            "evm.deployedBytecode.sourceMap",
-            "metadata",
-          ],
-        },
-      },
-    },
-  },
-  paths: {
-    sources: "./contracts/6",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
   },
   typechain: {
     outDir: "./typechain",
