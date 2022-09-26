1c0bc86c1cf8bd9dab3fdad1f036852a96c2e1de
Hoang Nguyen <hoangnguyen@fossil.com>
2022-09-26 20:58:54 +0700

    Change extension to .d.ts so debugging on vscode can work (running using hardhat don't require this change)

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-typechain");
+module.exports = {
+    defaultNetwork: "mainnet",
+    networks: {
+        mainnet: {
+            url: process.env.BSC_MAINNET_RPC,
+            accounts: [process.env.BSC_MAINNET_PRIVATE_KEY],
+            timeout: 1800000,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+};
+//# sourceMappingURL=hardhat.config.js.map
